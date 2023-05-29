c399c46e32518070d5bf0f724f94d30ebbd59c28
Juan Ayala <juandanielayalaquinones@gmail.com>
2023-05-29 10:48:02 -0500

    Nice

--- main.js
+++ main.js
@@ -2,12 +2,5 @@ const bars = document.querySelector(".bars");
 bars.onclick = function () {
   const navbar = document.querySelector(".navbar");
   navbar.classList.toggle("active");
-
 };
 
-const cargar=()=>{
-  for (let i = 0; i < 100; i++) {
-    console.log('@Darkwin');
-    
-  }
-}
